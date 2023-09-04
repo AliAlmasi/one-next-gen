786076450c252b06d68c69cb00c900e9f7d85187
Ali Almasi <A710almasi@gmail.com>
2023-09-05 01:35:36 +0330

    second commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const msgbox = document.querySelector(`.msgbox`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-msgbox`);
+const btnOpenModal = document.querySelectorAll(`.show-msgbox`);
+
+const closeModal = function () {
+	msgbox.classList.add(`hidden`);
+	overlay.classList.add(`hidden`);
+};
+
+const openModal = function () {
+	msgbox.classList.remove(`hidden`);
+	overlay.classList.remove(`hidden`);
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+	btnOpenModal[i].addEventListener(`click`, openModal);
+}
+
+btnCloseModal.addEventListener(`click`, closeModal);
+
+overlay.addEventListener(`click`, closeModal);
+
+document.addEventListener(`keydown`, function (e) {
+	if (e.key === "Escape" && !msgbox.classList.contains(`hidden`)) {
+		closeModal();
+	}
+});
+
+const generateRandomString = (length) =>
+	Math.random().toString(20).substr(2, length);
+
+const changeUUID = document.querySelector(`.changeUUid`);
+const codeEl = document.querySelector(`.side__code`);
+codeEl.textContent = generateRandomString(5);
+
+changeUUID.addEventListener(`click`, () => {
+	let x = generateRandomString(5);
+	codeEl.textContent = x;
+});
